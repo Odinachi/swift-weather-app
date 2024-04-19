3b6f7afa67526ad98a5af6599a9d80b24085a781
Odinachi David <odinachidavid@yahoo.com>
2024-04-19 23:04:10 +0100

    initial setup

--- sample-twos/ContentView.swift
+++ sample-twos/ContentView.swift
@@ -9,16 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+      ZStack {
+          Color(uiColor: UIColor(red: 112, green: 71, blue: 235)).ignoresSafeArea()
+          VStack
+          {
+              HStack(content: {
+                  /*@START_MENU_TOKEN@*/HStack {
+                      Image(systemName: "location.circle.fill")
+                      Text("Placeholder")
+                  }/*@END_MENU_TOKEN@*/
+              })
+          }
         }
-        .padding()
+      
     }
 }
 
+
+
 #Preview {
     ContentView()
+}
+
+
+extension UIColor {
+    convenience init(red: Int, green: Int, blue: Int, alpha: CGFloat = 1.0) {
+        self.init(
+            red: CGFloat(red) / 255.0,
+            green: CGFloat(green) / 255.0,
+            blue: CGFloat(blue) / 255.0,
+            alpha: alpha
+        )
+    }
 }
