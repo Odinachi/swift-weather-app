40dda7a7a027dd47c0bc553299da387d67e6f265
Odinachi David <odinachidavid@yahoo.com>
2024-04-19 22:03:00 +0100

    Initial Commit

---
+++ sample-twos/sample_twosApp.swift
@@ -0,0 +1,17 @@
+//
+//  sample_twosApp.swift
+//  sample-twos
+//
+//  Created by  Apple on 19/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct sample_twosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
