4d72f7ccd1892c2a5f932b49f3de3e4bbc802613
Odinachi David <odinachidavid@yahoo.com>
2024-04-20 01:19:14 +0100

    Quick fix

--- sample-twos/BottomBar.swift
+++ sample-twos/BottomBar.swift
@@ -5,4 +5,17 @@
 //  Created by  Apple on 20/04/2024.
 //
 
-import Foundation
+import SwiftUI
+
+
+struct BottomBar: View {
+    var body: some View {
+        HStack {
+            Spacer()
+            Text("Forecast report").foregroundColor(.white).font(.custom("", size: 18)).fontWeight(.black).padding(.trailing, 30)
+            Image(systemName: "chevron.up").foregroundColor(.white)
+            Spacer()
+        }.padding(.vertical, 20).background(.white.opacity(0.1)).cornerRadius(8)
+    }
+}
+
