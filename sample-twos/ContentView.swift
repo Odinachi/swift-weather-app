f2d4cc8debd3ec2bacbd0ef4d2c3296630092e58
Odinachi David <odinachidavid@yahoo.com>
2024-04-20 01:43:09 +0100

    refactor

--- sample-twos/ContentView.swift
+++ sample-twos/ContentView.swift
@@ -18,31 +18,7 @@ struct ContentView: View {
                 
                 TopBar()
                 Spacer()
-                HStack {
-                    Spacer()
-                    VStack(spacing: 0,content: {
-                        HStack{
-                            Image("cloud").resizable()
-                                .frame(width: 79, height: 66)
-                            VStack{
-                                Text("Today").foregroundColor(.white).font(.custom("", size: 24)).fontWeight(.bold)
-                                Text("Mon, 26 Apr").foregroundColor(.white).font(.custom("", size: 12)).fontWeight(.bold)
-                            }
-                        }
-                        HStack( content:{
-                           
-                            
-                            Text("28").foregroundColor(.white).font(.custom("", size: 158)).fontWeight(.bold)
-                            Text("°c").foregroundColor(.white).font(.custom("", size: 19)).fontWeight(.bold)  .padding(.bottom, 100)
-                        })
-                        Text("Lagos, Nigeria • 2:00 p.m").foregroundColor(.white).font(.custom("", size: 16)).fontWeight(.light)
-                    })
-                    Spacer()
-                }.padding(.vertical, 30).background(.white.opacity(0.1)).cornerRadius(15).overlay(
-                    RoundedRectangle(cornerRadius: 15)
-                        .stroke(Color(UIColor(red: 185, green: 188, blue: 242)), lineWidth: 1)
-                )
-                
+                CenterCard()
                 Spacer()
                 BottomBar()
             }.padding(20)
@@ -69,5 +45,4 @@ extension UIColor {
         )
     }
 }
-
 
