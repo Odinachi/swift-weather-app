7abd360fe7862411d9710d3079f844730314c431
Odinachi David <odinachidavid@yahoo.com>
2024-04-22 23:47:35 +0100

    Forcast report

--- sample-twos/Modals/ForcastModalView.swift
+++ sample-twos/Modals/ForcastModalView.swift
@@ -10,7 +10,10 @@ import SwiftUI
 struct ForcastModalView: View {
     @Environment(\.presentationMode) var presentationMode
     
-    let timeForcast: [[String: Any]] = [["temp": "29°c","isFull":false, "time":"8 am"],["temp": "29°c","isFull":false, "time":"10 am"],["temp": "29°c","isFull":true, "time":"12 am"],["temp": "28°c","isFull":true, "time":"2 pm"],["temp": "28°c","isFull":true, "time":"4 pm"],]
+    let timeForcast: [[String: Any]] = [["temp": "29°c","isFull":false, "time":"8 am"],["temp": "29°c","isFull":false, "time":"10 am"],["temp": "29°c","isFull":true, "time":"12 am"],["temp": "28°c","isFull":true, "time":"2 pm"],["temp": "28°c","isFull":true, "time":"4 pm"],]  
+    
+    
+    let dayForcast: [[String: Any]] = [["temp": "29°c","date":"April 5"],["temp": "29°c","date":"April 7"],["temp": "29°c", "date":"April 8"],["temp": "28°c", "date":"April 9"],["temp": "28°c", "date":"April 10"],]
     
     var body: some View {
         VStack(content: {
@@ -48,11 +51,41 @@ struct ForcastModalView: View {
             }).padding(.horizontal, 20).overlay(
                 RoundedRectangle(cornerRadius: 20)
                     .stroke(.border, lineWidth: 1)).padding(.horizontal, 20)
+//            Spacer()
+            HStack{
+                Text("Next forecast").frame(maxWidth: .infinity, alignment: .leading).font(.custom("DM Sans", size: 22)).fontWeight(.heavy).padding(.horizontal, 20).padding(.vertical,20)
+                Spacer()
+                Text("Five Days").foregroundColor(.white).font(.custom("DM Sans", size: 12)).padding(.horizontal, 20).padding(.vertical,
+                                                                                                                              10).background(.base).fontWeight(.bold).cornerRadius(8).frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 36)
+                
+               
+            }.padding(.trailing, 20)
+            
+       
+           VStack {
+                
+                ForEach(dayForcast.indices)
+                {
+                 
+                    i in HStack(alignment: .center,content: {
+                        Spacer()
+                        Text(dayForcast[i]["date"] as! String).font(.custom("DM Sans", size: 14)).fontWeight(.medium)
+                        
+                        Spacer()
+                        Image("HalfSun").frame(width: 26,height:26).padding(.vertical, 10)
+                        Spacer()
+                        Text(dayForcast[i]["temp"] as! String).font(.custom("DM Sans", size: 16)).fontWeight(.medium)
+                        Spacer()
+                    }).padding(.horizontal, 10).padding(.vertical,5)
+                    Divider()
+                    
+                }.padding(.horizontal, 5)
+            }.overlay(
+                RoundedRectangle(cornerRadius: 20)
+                    .stroke(.border, lineWidth: 1)).padding(.horizontal, 20)
             
             Spacer()
-            
-            
         })
-        .presentationDetents([.medium, .large]) .presentationCornerRadius(30).presentationDragIndicator(.hidden)
+        .presentationDetents([.large]) .presentationCornerRadius(30).presentationDragIndicator(.hidden)
     }
 }
